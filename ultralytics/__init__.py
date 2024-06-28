a29592784110e0621e2434a37d6fde5f3057a142
Tianlong Jia <tjian@gpu006.delftblue.tudelft.net>
2024-06-28 12:13:29 +0200

    train and test on pollutant dataset from Delftblue 2024.6.28

--- ultralytics/__init__.py
+++ ultralytics/__init__.py
@@ -1,8 +1,30 @@
-# Ultralytics YOLO 🚀, GPL-3.0 license
+# Ultralytics YOLO 🚀, AGPL-3.0 license
 
-__version__ = "8.0.36"
+__version__ = "8.2.44"
 
-from ultralytics.yolo.engine.model import YOLO
-from ultralytics.yolo.utils.checks import check_yolo as checks
+import os
 
-__all__ = ["__version__", "YOLO", "hub", "checks"]  # allow simpler import
+# Set ENV Variables (place before imports)
+os.environ["OMP_NUM_THREADS"] = "1"  # reduce CPU utilization during training
+
+from ultralytics.data.explorer.explorer import Explorer
+from ultralytics.models import NAS, RTDETR, SAM, YOLO, FastSAM, YOLOWorld
+from ultralytics.utils import ASSETS, SETTINGS
+from ultralytics.utils.checks import check_yolo as checks
+from ultralytics.utils.downloads import download
+
+settings = SETTINGS
+__all__ = (
+    "__version__",
+    "ASSETS",
+    "YOLO",
+    "YOLOWorld",
+    "NAS",
+    "SAM",
+    "FastSAM",
+    "RTDETR",
+    "checks",
+    "download",
+    "settings",
+    "Explorer",
+)
--- ultralytics/hub/__init__.py
+++ ultralytics/hub/__init__.py
@@ -1,98 +1,147 @@
-# Ultralytics YOLO 🚀, GPL-3.0 license
+# Ultralytics YOLO 🚀, AGPL-3.0 license
 
 import requests
 
+from ultralytics.data.utils import HUBDatasetStats
 from ultralytics.hub.auth import Auth
-from ultralytics.hub.session import HubTrainingSession
-from ultralytics.hub.utils import split_key
-from ultralytics.yolo.engine.exporter import EXPORT_FORMATS_LIST
-from ultralytics.yolo.engine.model import YOLO
-from ultralytics.yolo.utils import LOGGER, PREFIX, emojis
+from ultralytics.hub.session import HUBTrainingSession
+from ultralytics.hub.utils import HUB_API_ROOT, HUB_WEB_ROOT, PREFIX, events
+from ultralytics.utils import LOGGER, SETTINGS, checks
 
-# Define all export formats
-EXPORT_FORMATS_HUB = EXPORT_FORMATS_LIST + ["ultralytics_tflite", "ultralytics_coreml"]
+__all__ = (
+    "PREFIX",
+    "HUB_WEB_ROOT",
+    "HUBTrainingSession",
+    "login",
+    "logout",
+    "reset_model",
+    "export_fmts_hub",
+    "export_model",
+    "get_export",
+    "check_dataset",
+    "events",
+)
 
 
-def start(key=""):
+def login(api_key: str = None, save=True) -> bool:
     """
-    Start training models with Ultralytics HUB. Usage: from src.ultralytics import start; start('API_KEY')
+    Log in to the Ultralytics HUB API using the provided API key.
+
+    The session is not stored; a new session is created when needed using the saved SETTINGS or the HUB_API_KEY
+    environment variable if successfully authenticated.
+
+    Args:
+        api_key (str, optional): API key to use for authentication.
+            If not provided, it will be retrieved from SETTINGS or HUB_API_KEY environment variable.
+        save (bool, optional): Whether to save the API key to SETTINGS if authentication is successful.
+
+    Returns:
+        (bool): True if authentication is successful, False otherwise.
     """
-    auth = Auth(key)
-    try:
-        if not auth.get_state():
-            model_id = request_api_key(auth)
-        else:
-            _, model_id = split_key(key)
+    checks.check_requirements("hub-sdk>=0.0.8")
+    from hub_sdk import HUBClient
 
-        if not model_id:
-            raise ConnectionError(emojis('Connecting with global API key is not currently supported. ❌'))
+    api_key_url = f"{HUB_WEB_ROOT}/settings?tab=api+keys"  # set the redirect URL
+    saved_key = SETTINGS.get("api_key")
+    active_key = api_key or saved_key
+    credentials = {"api_key": active_key} if active_key and active_key != "" else None  # set credentials
 
-        session = HubTrainingSession(model_id=model_id, auth=auth)
-        session.check_disk_space()
+    client = HUBClient(credentials)  # initialize HUBClient
 
-        trainer = YOLO(session.input_file)
-        session.register_callbacks(trainer)
-        trainer.train(**session.train_args)
-    except Exception as e:
-        LOGGER.warning(f"{PREFIX}{e}")
+    if client.authenticated:
+        # Successfully authenticated with HUB
+
+        if save and client.api_key != saved_key:
+            SETTINGS.update({"api_key": client.api_key})  # update settings with valid API key
+
+        # Set message based on whether key was provided or retrieved from settings
+        log_message = (
+            "New authentication successful ✅" if client.api_key == api_key or not credentials else "Authenticated ✅"
+        )
+        LOGGER.info(f"{PREFIX}{log_message}")
+
+        return True
+    else:
+        # Failed to authenticate with HUB
+        LOGGER.info(f"{PREFIX}Get API key from {api_key_url} and then run 'yolo hub login API_KEY'")
+        return False
 
 
-def request_api_key(auth, max_attempts=3):
+def logout():
     """
-    Prompt the user to input their API key. Returns the model ID.
+    Log out of Ultralytics HUB by removing the API key from the settings file. To log in again, use 'yolo hub login'.
+
+    Example:
+        ```python
+        from ultralytics import hub
+
+        hub.logout()
+        ```
     """
-    import getpass
-    for attempts in range(max_attempts):
-        LOGGER.info(f"{PREFIX}Login. Attempt {attempts + 1} of {max_attempts}")
-        input_key = getpass.getpass("Enter your Ultralytics HUB API key:\n")
-        auth.api_key, model_id = split_key(input_key)
-
-        if auth.authenticate():
-            LOGGER.info(f"{PREFIX}Authenticated ✅")
-            return model_id
-
-        LOGGER.warning(f"{PREFIX}Invalid API key ⚠️\n")
-
-    raise ConnectionError(emojis(f"{PREFIX}Failed to authenticate ❌"))
+    SETTINGS["api_key"] = ""
+    SETTINGS.save()
+    LOGGER.info(f"{PREFIX}logged out ✅. To log in again, use 'yolo hub login'.")
 
 
-def reset_model(key=""):
-    # Reset a trained model to an untrained state
-    api_key, model_id = split_key(key)
-    r = requests.post("https://api.ultralytics.com/model-reset", json={"apiKey": api_key, "modelId": model_id})
-
+def reset_model(model_id=""):
+    """Reset a trained model to an untrained state."""
+    r = requests.post(f"{HUB_API_ROOT}/model-reset", json={"modelId": model_id}, headers={"x-api-key": Auth().api_key})
     if r.status_code == 200:
-        LOGGER.info(f"{PREFIX}model reset successfully")
+        LOGGER.info(f"{PREFIX}Model reset successfully")
         return
-    LOGGER.warning(f"{PREFIX}model reset failure {r.status_code} {r.reason}")
+    LOGGER.warning(f"{PREFIX}Model reset failure {r.status_code} {r.reason}")
 
 
-def export_model(key="", format="torchscript"):
-    # Export a model to all formats
-    assert format in EXPORT_FORMATS_HUB, f"Unsupported export format '{format}', valid formats are {EXPORT_FORMATS_HUB}"
-    api_key, model_id = split_key(key)
-    r = requests.post("https://api.ultralytics.com/export",
-                      json={
-                          "apiKey": api_key,
-                          "modelId": model_id,
-                          "format": format})
-    assert (r.status_code == 200), f"{PREFIX}{format} export failure {r.status_code} {r.reason}"
+def export_fmts_hub():
+    """Returns a list of HUB-supported export formats."""
+    from ultralytics.engine.exporter import export_formats
+
+    return list(export_formats()["Argument"][1:]) + ["ultralytics_tflite", "ultralytics_coreml"]
+
+
+def export_model(model_id="", format="torchscript"):
+    """Export a model to all formats."""
+    assert format in export_fmts_hub(), f"Unsupported export format '{format}', valid formats are {export_fmts_hub()}"
+    r = requests.post(
+        f"{HUB_API_ROOT}/v1/models/{model_id}/export", json={"format": format}, headers={"x-api-key": Auth().api_key}
+    )
+    assert r.status_code == 200, f"{PREFIX}{format} export failure {r.status_code} {r.reason}"
     LOGGER.info(f"{PREFIX}{format} export started ✅")
 
 
-def get_export(key="", format="torchscript"):
-    # Get an exported model dictionary with download URL
-    assert format in EXPORT_FORMATS_HUB, f"Unsupported export format '{format}', valid formats are {EXPORT_FORMATS_HUB}"
-    api_key, model_id = split_key(key)
-    r = requests.post("https://api.ultralytics.com/get-export",
-                      json={
-                          "apiKey": api_key,
-                          "modelId": model_id,
-                          "format": format})
-    assert (r.status_code == 200), f"{PREFIX}{format} get_export failure {r.status_code} {r.reason}"
+def get_export(model_id="", format="torchscript"):
+    """Get an exported model dictionary with download URL."""
+    assert format in export_fmts_hub(), f"Unsupported export format '{format}', valid formats are {export_fmts_hub()}"
+    r = requests.post(
+        f"{HUB_API_ROOT}/get-export",
+        json={"apiKey": Auth().api_key, "modelId": model_id, "format": format},
+        headers={"x-api-key": Auth().api_key},
+    )
+    assert r.status_code == 200, f"{PREFIX}{format} get_export failure {r.status_code} {r.reason}"
     return r.json()
 
 
-# temp. For checking
-if __name__ == "__main__":
-    start()
+def check_dataset(path: str, task: str) -> None:
+    """
+    Function for error-checking HUB dataset Zip file before upload. It checks a dataset for errors before it is uploaded
+    to the HUB. Usage examples are given below.
+
+    Args:
+        path (str): Path to data.zip (with data.yaml inside data.zip).
+        task (str): Dataset task. Options are 'detect', 'segment', 'pose', 'classify', 'obb'.
+
+    Example:
+        Download *.zip files from https://github.com/ultralytics/hub/tree/main/example_datasets
+            i.e. https://github.com/ultralytics/hub/raw/main/example_datasets/coco8.zip for coco8.zip.
+        ```python
+        from ultralytics.hub import check_dataset
+
+        check_dataset('path/to/coco8.zip', task='detect')  # detect dataset
+        check_dataset('path/to/coco8-seg.zip', task='segment')  # segment dataset
+        check_dataset('path/to/coco8-pose.zip', task='pose')  # pose dataset
+        check_dataset('path/to/dota8.zip', task='obb')  # OBB dataset
+        check_dataset('path/to/imagenet10.zip', task='classify')  # classification dataset
+        ```
+    """
+    HUBDatasetStats(path=path, task=task).get_json()
+    LOGGER.info(f"Checks completed correctly ✅. Upload this dataset to {HUB_WEB_ROOT}/datasets/.")
